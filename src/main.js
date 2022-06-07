9169061c0c10bda1f59d593a855435b64a4e8d45
snow <1529836290@qq.com>
2022-06-07 21:47:23 +0800

    更新

--- src/main.js
+++ src/main.js
@@ -7,6 +7,6 @@ import 'vant/lib/index.css';
 import lazyPlugin from 'vue3-lazy'
 import { Form, Field, CellGroup } from 'vant';
 createApp(App).use(lazyPlugin, {
-    loading: require('../img/imgLoading.png'), // 图片加载时默认图片
+    loading: require('../img/m_imgLoading.png'), // 图片加载时默认图片
     error: require('../img/imgFail.png')// 图片加载失败时默认图片
 }).use(router).use(Vant).use(Form).use(Field).use(CellGroup).mount('#app')
