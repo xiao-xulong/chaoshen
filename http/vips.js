4f935924f3ee134e9fc713be9be2e0a4c9b6b4ba
snow <1529836290@qq.com>
2022-07-03 19:01:15 +0800

    更新

--- http/vips.js
+++ http/vips.js
@@ -40,7 +40,8 @@ const vips = [{ qq: '2592526921', name: '靜', money: '50' },
 { qq: '2214075596', name: 'WIND', money: '3' },
 { qq: '296252098', name: '寒殇', money: '3' },
 { qq: '2283703730', name: '无冕之王', money: '3' },
-{ qq: '209333524', name: '神话', money: '3' }
+{ qq: '209333524', name: '神话', money: '3' },
+{ qq: '2786713799', name: 'Einsa.', money: '3' }
 
 
 ]
