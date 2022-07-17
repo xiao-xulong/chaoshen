ef269f00af3a6ca0be37177c2b5cda0f92757776
snow <1529836290@qq.com>
2022-07-17 18:52:30 +0800

    完成

--- src/views/xiongThree.vue
+++ src/views/xiongThree.vue
@@ -1,76 +1,90 @@
 <template>
-    <div class="root">
-        <div @click="menu">
-            <pageTop>
-            </pageTop>
-        </div>
-
-        <div class="video">
-            <img :src="icon" v-if="!videoPlayState" class="icon" @click="play">
-            <video ref="v" crossorigin="anonymous" x5-video-player-type="h5" :src="videoAddress" :poster="poster"
-                @click="pause" @timeupdate="timeU" @canplay="timeW" @ended="videoEnd">
-
-            </video>
-            <div class="control-bar">
-
-                <span class="times"> {{
-                        playCurrentTime
-                }} / {{ TimeD }}</span>
-
-                <van-slider active-color="#fb7299" v-model="RangeValue" @change="timeC" :max="videoDuraction" />
-                <i @click="fullScreen" class="fullScreenIcon"><svg viewBox="0 0 22 22"
-                        xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                        <defs>
-                            <path
-                                d="M17.262 9.995a.75.75 0 01.75.75v6.585a.75.75 0 01-.75.75h-6.515a.75.75 0 010-1.5h5.765v-5.835a.75.75 0 01.75-.75zm-6-6a.75.75 0 110 1.5H5.497v5.835a.75.75 0 01-1.5 0V4.745a.75.75 0 01.75-.75h6.515z"
-                                id="pid-2-svgo-a"></path>
-                        </defs>
-                        <g fill="none" fill-rule="evenodd">
-                            <path d="M0 0h22v22H0z"></path>
-                            <mask id="pid-2-svgo-b" fill="#fff">
-                                <use xlink:href="#pid-2-svgo-a"></use>
-                            </mask>
-                            <use fill="#979797" fill-rule="nonzero" xlink:href="#pid-2-svgo-a"></use>
-                            <g mask="url(#pid-2-svgo-b)" fill="#FFF" fill-rule="nonzero">
-                                <path d="M0 0h22v22H0z"></path>
-                            </g>
-                        </g>
-                    </svg></i>
-
-            </div>
-        </div>
-        <div class="info">
-            <img src="../assets/webInfoImg.jpg">
-            <div class="text">
-                <div class="text_title">
-                    <span class="name">雄兵连3雷霆万钧</span>
-                    <span class="point">9.0分</span>
-                </div>
-                <div class="content">
-                    <p> 连载中</p>
-                    雄兵连为国内首部3D军事科幻类动漫作品，《雄兵连》描述了在神河宇宙中各星系文明与神秘的虚空世界之间的发生的科幻战争故事。
-                </div>
-            </div>
-
-
-
-        </div>
-        <div class="ep-list">
-            <div class="ep-list-header">
-                <span class="ep-title"> 选集</span>
-                <span class="ep-last">更新至第10话</span>
-            </div>
-            <div class="ep-list-body">
-                <div :class="['ep-item', choosedEp == item.id ? 'ep-item-active' : '']" v-for="item in eps" :key="item"
-                    @click="changeEp(item)">
-                    {{ item.name }}
-                </div>
-            </div>
-        </div>
-
-
-
+  <div class="root">
+    <div @click="menu">
+      <pageTop> </pageTop>
     </div>
+
+    <div class="video">
+      <img :src="icon" v-if="!videoPlayState" class="icon" @click="play" />
+      <video
+        ref="v"
+        crossorigin="anonymous"
+        x5-video-player-type="h5"
+        :src="videoAddress"
+        :poster="poster"
+        @click="pause"
+        @timeupdate="timeU"
+        @canplay="timeW"
+        @ended="videoEnd"
+      ></video>
+      <div class="control-bar">
+        <span class="times"> {{ playCurrentTime }} / {{ TimeD }}</span>
+
+        <van-slider
+          active-color="#fb7299"
+          v-model="RangeValue"
+          @change="timeC"
+          :max="videoDuraction"
+        />
+        <i @click="fullScreen" class="fullScreenIcon"
+          ><svg
+            viewBox="0 0 22 22"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+          >
+            <defs>
+              <path
+                d="M17.262 9.995a.75.75 0 01.75.75v6.585a.75.75 0 01-.75.75h-6.515a.75.75 0 010-1.5h5.765v-5.835a.75.75 0 01.75-.75zm-6-6a.75.75 0 110 1.5H5.497v5.835a.75.75 0 01-1.5 0V4.745a.75.75 0 01.75-.75h6.515z"
+                id="pid-2-svgo-a"
+              ></path>
+            </defs>
+            <g fill="none" fill-rule="evenodd">
+              <path d="M0 0h22v22H0z"></path>
+              <mask id="pid-2-svgo-b" fill="#fff">
+                <use xlink:href="#pid-2-svgo-a"></use>
+              </mask>
+              <use
+                fill="#979797"
+                fill-rule="nonzero"
+                xlink:href="#pid-2-svgo-a"
+              ></use>
+              <g mask="url(#pid-2-svgo-b)" fill="#FFF" fill-rule="nonzero">
+                <path d="M0 0h22v22H0z"></path>
+              </g>
+            </g></svg
+        ></i>
+      </div>
+    </div>
+    <div class="info">
+      <img src="../assets/webInfoImg.jpg" />
+      <div class="text">
+        <div class="text_title">
+          <span class="name">雄兵连3雷霆万钧</span>
+          <span class="point">9.0分</span>
+        </div>
+        <div class="content">
+          <p>连载中</p>
+          雄兵连为国内首部3D军事科幻类动漫作品，《雄兵连》描述了在神河宇宙中各星系文明与神秘的虚空世界之间的发生的科幻战争故事。
+        </div>
+      </div>
+    </div>
+    <div class="ep-list">
+      <div class="ep-list-header">
+        <span class="ep-title"> 选集</span>
+        <span class="ep-last">更新至第11话</span>
+      </div>
+      <div class="ep-list-body">
+        <div
+          :class="['ep-item', choosedEp == item.id ? 'ep-item-active' : '']"
+          v-for="item in eps"
+          :key="item"
+          @click="changeEp(item)"
+        >
+          {{ item.name }}
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script >
@@ -110,6 +124,7 @@ export default {
         { id: 7, name: '第7话', videoAddress: 'http://42.192.47.115:16880/xs07.mp4' },
         { id: 9, name: '第9话', videoAddress: 'http://42.192.47.115:16880/xs09.mp4' },
         { id: 10, name: '第10话', videoAddress: 'http://42.192.47.115:16880/xs10.mp4' }
+               { id: 10, name: '第11话', videoAddress: 'http://42.192.47.115:16880/xs11.mp4' }
             // { id: 3, name: '第3话' }, 
             // { id: 4, name: '第4话' }, 
             // { id: 5, name: '第5话' }
@@ -188,210 +203,203 @@ export default {
 
 <style  scoped lang="less">
 .root {
-    background-color: #ffffff;
-    height: 100%;
+  background-color: #ffffff;
+  height: 100%;
 }
 
 .menu {
-    z-index: 10;
+  z-index: 10;
 }
 
 .video {
-    position: relative;
-    width: 100%;
-
+  position: relative;
+  width: 100%;
 }
 
 video {
-    width: 100%;
-    object-fit: fill;
+  width: 100%;
+  object-fit: fill;
 }
 
 .icon {
-    z-index: 5;
-    position: absolute;
-    width: 99px;
-    height: 90px;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+  z-index: 5;
+  position: absolute;
+  width: 99px;
+  height: 90px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 
 .info {
-    width: 100%;
-    border-bottom: 1px solid #e7e7e7;
+  width: 100%;
+  border-bottom: 1px solid #e7e7e7;
+  display: flex;
+  flex-wrap: nowrap;
+  padding-left: 10px;
+  padding-right: 10px;
+  padding-bottom: 10px;
+  box-sizing: border-box;
+  margin-top: 10px;
+
+  img {
+    width: 25%;
+    height: 250px;
+  }
+
+  .text {
+    width: 75%;
     display: flex;
-    flex-wrap: nowrap;
+    flex-direction: column;
+    justify-content: space-between;
     padding-left: 10px;
     padding-right: 10px;
-    padding-bottom: 10px;
     box-sizing: border-box;
-    margin-top: 10px;
 
-    img {
-        width: 25%;
-        height: 250px;
+    .text_title {
+      display: flex;
+      justify-content: space-between;
+
+      .name {
+        display: inline-block;
+        font-size: 32px;
+      }
+
+      .point {
+        display: inline-block;
+        color: #ffa726;
+        font-size: 40px;
+      }
     }
 
-    .text {
-        width: 75%;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        padding-left: 10px;
-        padding-right: 10px;
-        box-sizing: border-box;
-
-        .text_title {
-            display: flex;
-            justify-content: space-between;
-
-            .name {
-                display: inline-block;
-                font-size: 32px;
-            }
-
-            .point {
-                display: inline-block;
-                color: #ffa726;
-                font-size: 40px;
-            }
-        }
-
-        .content {
-            font-size: 12Px;
-            color: #999;
-            line-height: 40px;
-        }
-
+    .content {
+      font-size: 12px;
+      color: #999;
+      line-height: 40px;
     }
+  }
 
-
-
-    /* https://puui.qpic.cn/vcover_vt_pic/0/mzc002006zef3n41652588676293/260 */
-
+  /* https://puui.qpic.cn/vcover_vt_pic/0/mzc002006zef3n41652588676293/260 */
 }
 
 .ep-list {
-    margin-top: 10px;
-    border-bottom: 1px solid #ccc;
-    padding-bottom: 15px;
+  margin-top: 10px;
+  border-bottom: 1px solid #ccc;
+  padding-bottom: 15px;
+  box-sizing: border-box;
+
+  .ep-list-header {
+    display: flex;
+    height: 60px;
+    margin-bottom: 10px;
+    justify-content: space-between;
+    padding-left: 15px;
+    padding-right: 15px;
     box-sizing: border-box;
 
-    .ep-list-header {
-        display: flex;
-        height: 60px;
-        margin-bottom: 10px;
-        justify-content: space-between;
-        padding-left: 15px;
-        padding-right: 15px;
-        box-sizing: border-box;
-
-        .ep-title {
-            font-size: 36px;
-            line-height: 60px;
-            font-weight: 700;
-        }
-
-        .ep-last {
-            color: #999;
-            line-height: 60px;
-            font-size: 28px;
-        }
-
-        .ep-last::after {
-            content: "";
-            // position: relative;
-            // top: 50%;
-            // transform: translateY(-50%);
-            margin-top: 21px;
-            display: inline-block;
-            vertical-align: top;
-            width: 15px;
-            height: 15px;
-            border-top: 1px solid #999;
-            border-right: 1px solid #999;
-            -webkit-transform: rotate(45deg);
-            -ms-transform: rotate(45deg);
-            transform: rotate(45deg);
-            // margin-top: .74666667rem;
-            // margin-right: .10666667rem;
-            // margin-left: .10666667rem;
-            position: relative;
-        }
+    .ep-title {
+      font-size: 36px;
+      line-height: 60px;
+      font-weight: 700;
     }
 
-    .ep-list-body {
-        white-space: nowrap;
-        overflow-x: scroll;
-        height: 100px;
-
-        .ep-item {
-            width: 256px;
-            height: 86px;
-            line-height: 86px;
-            text-align: center;
-            border-radius: .21333333rem;
-            border: 1px solid #ccc;
-            display: inline-block;
-            margin-left: 20px;
-            font-size: 28px;
-            letter-spacing: 5px;
-        }
-
-        .ep-item-active {
-            border-color: #fb7299;
-            color: #fb7299;
-        }
+    .ep-last {
+      color: #999;
+      line-height: 60px;
+      font-size: 28px;
     }
 
-    .ep-list-body::-webkit-scrollbar {
-        display: none;
+    .ep-last::after {
+      content: "";
+      // position: relative;
+      // top: 50%;
+      // transform: translateY(-50%);
+      margin-top: 21px;
+      display: inline-block;
+      vertical-align: top;
+      width: 15px;
+      height: 15px;
+      border-top: 1px solid #999;
+      border-right: 1px solid #999;
+      -webkit-transform: rotate(45deg);
+      -ms-transform: rotate(45deg);
+      transform: rotate(45deg);
+      // margin-top: .74666667rem;
+      // margin-right: .10666667rem;
+      // margin-left: .10666667rem;
+      position: relative;
     }
+  }
+
+  .ep-list-body {
+    white-space: nowrap;
+    overflow-x: scroll;
+    height: 100px;
+
+    .ep-item {
+      width: 256px;
+      height: 86px;
+      line-height: 86px;
+      text-align: center;
+      border-radius: 0.21333333rem;
+      border: 1px solid #ccc;
+      display: inline-block;
+      margin-left: 20px;
+      font-size: 28px;
+      letter-spacing: 5px;
+    }
+
+    .ep-item-active {
+      border-color: #fb7299;
+      color: #fb7299;
+    }
+  }
+
+  .ep-list-body::-webkit-scrollbar {
+    display: none;
+  }
 }
 
 .control-bar {
-    padding-left: 10px;
-    height: 40px;
-    position: absolute;
-    z-index: 2;
-    bottom: 15px;
-    left: 0;
-    right: 0;
-    border: 0 solid #e2e2e2;
-    background-color: rgba(0, 0, 0, .4);
-    opacity: 1;
-    line-height: 40px;
+  padding-left: 10px;
+  height: 40px;
+  position: absolute;
+  z-index: 2;
+  bottom: 15px;
+  left: 0;
+  right: 0;
+  border: 0 solid #e2e2e2;
+  background-color: rgba(0, 0, 0, 0.4);
+  opacity: 1;
+  line-height: 40px;
 
-    .times {
-        display: inline-block;
-        height: 100%;
-        line-height: 60px;
-        margin-right: 20px;
-        font-size: 28px;
-        color: #ffffff;
-
-    }
+  .times {
+    display: inline-block;
+    height: 100%;
+    line-height: 60px;
+    margin-right: 20px;
+    font-size: 28px;
+    color: #ffffff;
+  }
 }
 
-
 :deep(.van-slider) {
-    display: inline-block;
-    width: 60%;
-    height: 10px;
-    position: relative;
-    bottom: 5px;
+  display: inline-block;
+  width: 60%;
+  height: 10px;
+  position: relative;
+  bottom: 5px;
 }
 
 .fullScreenIcon {
-    display: inline-block;
-    width: 40px;
-    height: 40px;
-    font-weight: 400;
-    font-style: normal;
-    -webkit-font-smoothing: antialiased;
-    position: relative;
-    left: 45px;
-    top: 8px;
+  display: inline-block;
+  width: 40px;
+  height: 40px;
+  font-weight: 400;
+  font-style: normal;
+  -webkit-font-smoothing: antialiased;
+  position: relative;
+  left: 45px;
+  top: 8px;
 }
 </style>
