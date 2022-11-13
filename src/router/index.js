b8ae201def7026101692d7fe02cad2bd41c4a140
肖旭龙 <xiaoxulong@aa.com>
2022-11-13 12:40:17 +0800

    完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,56 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-
-const routes = [
-  {
-    path: '/',
-    name: 'Login',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
-  },
-  {
-    path: '/Home',
-    name: 'Home',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue')
-  },
-  {
-    path: '/WallPaper',
-    name: 'WallPaper',
-    component: () => import(/* webpackChunkName: "about" */ '../views/WallPaper.vue')
-  },
-  {
-    path: '/PicDownload',
-    name: 'PicDownload',
-    component: () => import(/* webpackChunkName: "about" */ '../views/PicDownload.vue')
-  }, {
-    path: '/Us',
-    name: 'Us',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Us.vue')
-  },
-  {
-    path: '/Share',
-    name: 'Share',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Share.vue')
-  },
-  {
-    path: '/QPics',
-    name: 'QPics',
-    component: () => import(/* webpackChunkName: "about" */ '../views/QPics.vue')
-  },
-  {
-    path: '/xiongThree',
-    name: 'xiongThree',
-    component: () => import(/* webpackChunkName: "about" */ '../views/xiongThree.vue')
-  },
-  {
-    path: '/activePics',
-    name: 'activePics',
-    component: () => import(/* webpackChunkName: "about" */ '../views/activePics.vue')
-  },
-  {
-    path: '/activePicsDownload',
-    name: 'activePicsDownload',
-    component: () => import(/* webpackChunkName: "about" */ '../views/activePicsDownload.vue')
-  }
-]
+import { chaoshen } from './routerInfo/routerInfo'
+const routes = [...chaoshen]
 
 const router = createRouter({
   history: createWebHashHistory(),
@@ -59,8 +9,7 @@ const router = createRouter({
 router.beforeEach((to, from, next) => {
 
   if (to.path === '/') {
-    // console.log('a十大大苏打的')
-    // console.log(to.path)
+
     return next()
   } else {
     const token = window.sessionStorage.getItem('angle')
---
+++ src/router/routerInfo/routerInfo.js
@@ -0,0 +1,52 @@
+const chaoshen = [
+  {
+    path: '/',
+    name: 'Login',
+    component: () => import(/* webpackChunkName: "about" */ '../../views/Login.vue')
+  },
+  {
+    path: '/Home',
+    name: 'Home',
+    component: () => import(/* webpackChunkName: "about" */ '../../views/Home.vue')
+  },
+  {
+    path: '/WallPaper',
+    name: 'WallPaper',
+    component: () => import(/* webpackChunkName: "about" */ '../../views/WallPaper.vue')
+  },
+  {
+    path: '/PicDownload',
+    name: 'PicDownload',
+    component: () => import(/* webpackChunkName: "about" */ '../../views/PicDownload.vue')
+  }, {
+    path: '/Us',
+    name: 'Us',
+    component: () => import(/* webpackChunkName: "about" */ '../../views/Us.vue')
+  },
+  {
+    path: '/Share',
+    name: 'Share',
+    component: () => import(/* webpackChunkName: "about" */ '../../views/Share.vue')
+  },
+  {
+    path: '/QPics',
+    name: 'QPics',
+    component: () => import(/* webpackChunkName: "about" */ '../../views/QPics.vue')
+  },
+  {
+    path: '/xiongThree',
+    name: 'xiongThree',
+    component: () => import(/* webpackChunkName: "about" */ '../../views/xiongThree.vue')
+  },
+  {
+    path: '/activePics',
+    name: 'activePics',
+    component: () => import(/* webpackChunkName: "about" */ '../../views/activePics.vue')
+  },
+  {
+    path: '/activePicsDownload',
+    name: 'activePicsDownload',
+    component: () => import(/* webpackChunkName: "about" */ '../../views/activePicsDownload.vue')
+  }
+]
+export { chaoshen }
