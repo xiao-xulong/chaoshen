92d0e360e23aa8c400e56dfd53fa0cf1d99fa53d
肖旭龙 <xiaoxulong@aa.com>
2023-07-04 10:30:36 +0800

    local

--- http/http.js
+++ http/http.js
@@ -336,6 +336,7 @@ const examplePics = [
 const allPics = [
 
   // {url:'',name:''},
+  { url: require('../img/mainIng/AI雪伊(2).jpg'), name: 'AI雪伊(2)' },
   { url: require('../img/mainIng/AI凉冰(3).jpg'), name: 'AI凉冰(3)' },
   { url: require('../img/mainIng/AI雪伊.jpg'), name: 'AI雪伊' },
   { url: require('../img/mainIng/AI彦(37).jpg'), name: 'AI彦(37)' },
