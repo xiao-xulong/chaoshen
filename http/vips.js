4257ed94084044b3310f224799535a0da5fc0ede
肖旭龙 <xiaoxulong@aa.com>
2023-07-05 20:09:34 +0800

    ai

--- http/vips.js
+++ http/vips.js
@@ -149,5 +149,6 @@ const vips = [
   { qq: "2632172039", name: "哦吼", money: "3" },
   { qq: "3437153883", name: "嗨嗨嗨", money: "3" },
   { qq: "2095437603", name: "七天银爵", money: "3" },
+  { qq: "1978899728", name: "拾忆最初的梦想", money: "3" },
 ];
 export { vips };
