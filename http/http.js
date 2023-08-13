0a92f5734b07944e5108fe8bf552c94ac298d5be
肖旭龙 <xiaoxulong@aa.com>
2023-08-13 19:52:05 +0800

    local

--- http/http.js
+++ http/http.js
@@ -336,7 +336,14 @@ const examplePics = [
 const allPics = [
 
   // {url:'',name:''},
-
+  { url: require('../img/mainIng/AI鹤熙(78).jpg'), name: 'AI鹤熙(78)' },
+  { url: require('../img/mainIng/AI琪琳(33).jpg'), name: 'AI琪琳(33)' },
+  { url: require('../img/mainIng/AI琪琳(34).jpg'), name: 'AI琪琳(34)' },
+  { url: require('../img/mainIng/AI琪琳(35).jpg'), name: 'AI琪琳(35)' },
+  { url: require('../img/mainIng/AI琪琳(36).jpg'), name: 'AI琪琳(36)' },
+  { url: require('../img/mainIng/AI琪琳(37).jpg'), name: 'AI琪琳(37)' },
+  { url: require('../img/mainIng/AI琪琳(38).jpg'), name: 'AI琪琳(38)' },
+  { url: require('../img/mainIng/AI琪琳(39).jpg'), name: 'AI琪琳(39)' },
   { url: require('../img/mainIng/AI彦(61).jpg'), name: 'AI彦(61)' },
   { url: require('../img/mainIng/AI彦(62).jpg'), name: 'AI彦(62)' },
   { url: require('../img/mainIng/AI凯莎(5).jpg'), name: 'AI凯莎(5)' },
